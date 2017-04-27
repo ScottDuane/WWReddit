aab4bccde31c193cb6e5833df8b060f88f62c851
Scott Duane <adrian.scott.duane@gmail.com>
2017-04-26 18:47:19 -0700

    Add docs folder for github pages

---
+++ actions.js
@@ -0,0 +1,16 @@
+// getSuggestedSubreddits
+// getSubreddit
+//
+
+let getSuggestedSubreddits() = () => {
+  // takes the user's current subs and grabs all suggestions
+  // returns all, to later be shuffled and parsed by a different action
+}
+
+let getSubreddit() = (srName) => {
+  // performs an AJAX request to the Reddit api to grab the subreddit of the given srName
+}
+
+let addSubreddit() = (srName) => {
+  // 
+}
